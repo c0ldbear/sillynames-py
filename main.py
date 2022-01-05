ef77574a9bcabff55987cf6e32eed7b40d8d3a7e
teddy <teddyjh@protonmail.com>
2022-01-05 12:38:37 +0100

    generate a random silly name and present it to the terminal

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
+import random
 
 # Open and read 'first.txt' and 'last.txt'
 fFirst = open('./data/first.txt', 'r') # reads file 'first.txt'
-first = [name.replace(',\n', '') for name in fFirst.readlines()] # cleans the data from ',\n' after the names
+firstList = [name.replace(',\n', '') for name in fFirst.readlines()] # cleans the data from ',\n' after the names
 
 fLast = open('./data/last.txt', 'r') # reads file 'first.txt'
-last = [name.replace(',\n', '') for name in fLast.readlines()]
+lastList = [name.replace(',\n', '') for name in fLast.readlines()]
 
 # Randomly select a first and lastname
 
+first = random.choice(firstList)
+last = random.choice(lastList)
+
 # Present the randomly generated name
+print("A new name is chosen: {} {}".format(first, last))
 
 # Option: Add 'user input' as to ask if they want to get a new name -> Repeat
