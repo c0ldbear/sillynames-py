3b7db4956ca2917e058810b28f9ccfa6dfd8bba6
teddy <teddyjh@protonmail.com>
2022-01-05 12:27:46 +0100

    git init and add main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+
+# Open and read 'first.txt' and 'last.txt'
+
+# Randomly select a first and lastname
+
+# Present the randomly generated name
+
+# Option: Add 'user input' as to ask if they want to get a new name -> Repeat
