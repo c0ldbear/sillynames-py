1045e5f40d5ede76f71b17ffeb58c780d5f0d4b2
teddy <teddyjh@protonmail.com>
2022-01-05 12:33:11 +0100

    read files of names and clean the names from unwanted char

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 
 # Open and read 'first.txt' and 'last.txt'
+fFirst = open('./data/first.txt', 'r') # reads file 'first.txt'
+first = [name.replace(',\n', '') for name in fFirst.readlines()] # cleans the data from ',\n' after the names
+
+fLast = open('./data/last.txt', 'r') # reads file 'first.txt'
+last = [name.replace(',\n', '') for name in fLast.readlines()]
 
 # Randomly select a first and lastname
 
