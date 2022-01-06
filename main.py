23714f6e1a9f1966c046ba668ec539904b74f294
teddy <teddyjh@protonmail.com>
2022-01-06 11:54:14 +0100

    refactor to have 'main' fcn first then the other helper fcns

--- main.py
+++ main.py
@@ -1,24 +1,5 @@
 import random
 
-def GetListOfNamesFromFile(filename):
-    file = open(filename, 'r')
-    nameList = [name.replace(',\n', '') for name in file.readlines()]
-    file.close() # Don't forget to close the file once done
-    return nameList
-
-def GetRandomNameFromList(listOfNames):
-    return random.choice(listOfNames)
-
-def DisplayFullName(first, last):
-        print("A name is chosen: {} {}".format(first, last))
-
-def ContinueOrExitApp():
-    choice = input("Do you want another name? (Press 'Enter' else n or q to quit)\n")
-    if choice.lower() == 'n' or choice.lower() == 'q':
-        print('Exiting...')
-        return True 
-    return False
-
 def main():
     # Open and read 'first.txt' and 'last.txt'
     firstnamesFilename = './data/first.txt'
@@ -38,6 +19,25 @@ def main():
         if ContinueOrExitApp():
             break
 
-# safe way of running python apps (good to have as a habit)
+def GetListOfNamesFromFile(filename):
+    file = open(filename, 'r')
+    nameList = [name.replace(',\n', '') for name in file.readlines()]
+    file.close() # Don't forget to close the file once done
+    return nameList
+
+def GetRandomNameFromList(listOfNames):
+    return random.choice(listOfNames)
+
+def DisplayFullName(first, last):
+        print("A name is chosen: {} {}".format(first, last))
+
+def ContinueOrExitApp():
+    choice = input("Do you want another name? (Press 'Enter' else n or q to quit)\n")
+    if choice.lower() == 'n' or choice.lower() == 'q':
+        print('Exiting...')
+        return True 
+    return False
+
+# safer way of running python apps (good to have as a habit)
 if __name__ == '__main__':
     main()
