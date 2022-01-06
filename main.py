2dc790a3f1bfaa7272bebe2af9c9698a7feecd7a
teddy <teddyjh@protonmail.com>
2022-01-06 11:48:25 +0100

    refactor with a new fcn for randomly select a name from list of names

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
+import random
+
 def GetNamesFromFile(filename):
     file = open(filename, 'r')
     nameList = [name.replace(',\n', '') for name in file.readlines()]
     file.close() # Don't forget to close the file once done
     return nameList
 
-def main():
-    import random
+def GetRandomNameFromList(listOfNames):
+    return random.choice(listOfNames)
 
+def main():
     # Open and read 'first.txt' and 'last.txt'
     firstnamesFilename = './data/first.txt'
     lastnamesFilename = './data/last.txt'
@@ -15,8 +18,8 @@ def main():
 
     # Randomly select a first and lastname
     while True:
-        first = random.choice(firstList)
-        last = random.choice(lastList)
+        first = GetRandomNameFromList(firstList)
+        last = GetRandomNameFromList(lastList)
 
         # Present the randomly generated name
         print("A name is chosen: {} {}".format(first, last))
